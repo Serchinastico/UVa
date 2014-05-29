7a1c5b34a8c917f952274068b7ee738378302e4e
Serchinastico <54 muchas gafas nadan>
2014-05-29 16:30:24 +0200

    Removed invalid solution for 10830

--- 10830.cpp
+++ 10830.cpp
@@ -6,82 +6,32 @@
 
 using namespace std;
 
-void addNewPrime(int *primes, int &lastPrimeIndex) {
-	bool hasFoundNewPrime = false;
-	int newPrimeCandidate = primes[lastPrimeIndex];
-	while (!hasFoundNewPrime) {
-		newPrimeCandidate += 1; // TODO Odd numbers...
-		bool isPrime = true;
-		for (int iPrime = 0; iPrime <= lastPrimeIndex; iPrime++) {
-			int prime = primes[iPrime];
-			if (prime * prime > newPrimeCandidate) {
-				break;
-			} else if (newPrimeCandidate % prime == 0) {
-				isPrime = false;
-				break;
-			}
+unsigned long long cumulativeSumOfDivisors(unsigned long value) {
+	unsigned long long cumulativeSumOfDivisors = 0;
+	unsigned long long limit = sqrt(value);
+
+	for (unsigned long i = 2; i <= limit; i++) {
+		unsigned long long a = ceil(value / (i + 1));
+		unsigned long long b = floor(value / i);
+
+		if (a == 0) break;
+
+		unsigned long long sum = (i - 1) * (((b * (b + 1)) / 2) - ((a * (a + 1)) / 2));
+		cumulativeSumOfDivisors += sum;
+		if (b != i) {
+			cumulativeSumOfDivisors += (b - 1) * i;
 		}
-
-		if (isPrime) {
-			primes[lastPrimeIndex + 1] = newPrimeCandidate;
-			lastPrimeIndex += 1;
-			hasFoundNewPrime = true;
-		}
-	}
-}
-
-unsigned long sumOfDivisors(unsigned long value, int *primes, int lastPrimeIndex) {
-	unsigned long sumOfDivisors = 1;
-
-	unsigned long remain = value;
-	for (int iPrime = 0; iPrime < lastPrimeIndex; iPrime++) {
-		int prime = primes[iPrime];
-		if (prime * prime > value) {
-			break;
-		}
-
-		int exponent = 0;
-		while (remain % prime == 0) {
-			remain /= prime;
-			exponent += 1;
-		}
-		sumOfDivisors *= (pow(prime, exponent + 1) - 1) / (prime - 1);
-	}
-
-	if (remain > 1) {
-		sumOfDivisors *= (remain * remain - 1) / (remain - 1);
-	}
-	
-	return sumOfDivisors - (value + 1);
-}
-
-unsigned long cumulativeSumOfDivisors(unsigned long value, int *primes, int lastPrimeIndex) {
-	unsigned long lastPrime = primes[lastPrimeIndex];
-	while (lastPrime * lastPrime <= value) {
-		addNewPrime(primes, lastPrimeIndex);
-		lastPrime = primes[lastPrimeIndex];
-	}
-
-	unsigned long cumulativeSumOfDivisors = 0;
-	for (unsigned long i = 2; i <= value; i++) {
-		cumulativeSumOfDivisors += sumOfDivisors(i, primes, lastPrimeIndex);
 	}
 
 	return cumulativeSumOfDivisors;
 }
 
 int main() {
-	int primes[5000];
-	primes[0] = 2; primes[1] = 3;
-	primes[2] = 5; primes[3] = 7;
-	primes[4] = 11; primes[5] = 13;
-	primes[6] = 17; primes[7] = 19;
-	int lastPrimeIndex = 7;
-
 	unsigned long value;
 	int index = 1;
+
 	while (cin >> value && value != 0) {
-		cout << "Case " << index -1 << ": " << cumulativeSumOfDivisors(value, primes, lastPrimeIndex) << endl;
+		cout << "Case " << index  << ": " << cumulativeSumOfDivisors(value) << endl;
 		index += 1;
 	}
 }
